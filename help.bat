0378175d6383eaac24fd66906504a24a8b965f4e
Zack "Zwacky" P <piggythepigcraft@gmail.com>
2016-07-28 06:50:20 -0600

    HELP CMD

--- help.bat
+++ help.bat
@@ -4,3 +4,9 @@ echo  > Shows a list of CMDS
 echo.
 echo > clr
 echo  > Clears terminal
+echo.
+echo > info
+echo  > Displays info of user and OS
+echo.
+echo > atc
+echo  > Shows the attachments of the CMD.
